1fdd91f68bdcf3d010a1f0c5d2dd556592d9042a
vdbaan <steven@vdbaan.net>
2014-09-03 14:40:16 +0100

    test

--- src/test/jython/PythonPoc.py
+++ src/test/jython/PythonPoc.py
@@ -1,6 +1,6 @@
 __author__ = 'steven'
 import sys
 
-my_file = open('src/test/resources/file.txt','w')
+my_file = open('src/test/resources/file.txt', 'w')
 lines = my_file.readlines()
-sys.stdout.println("The contents of the file is: %s" %lines)
+sys.stdout.println("The contents of the file is: %s" % lines)
